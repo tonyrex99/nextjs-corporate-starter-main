652941f1c8f86fcd91a17d2263954ca5b5a7e58a
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2024-01-07 00:29:16 +0100

    fixed db

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOSTa", "127.0.0.1"),
-      port: env.int("DATABASE_PORTa", 3306),
-      database: env("DATABASE_NAMEa", "strapi2"),
-      user: env("DATABASE_USERNAMEa", "root"),
-      password: env("DATABASE_PASSWORDa", "femwak69"),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "femwak69"),
       ssl: false,
     },
     debug: false,
